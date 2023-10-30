294e4c080aa813af85318545bbe6dc5a80fed3e5
İdil Güler <idilguler@Idil-MacBook-Pro-2.local>
2023-10-30 15:31:38 +0300

    web code initalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'; 
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 import './App.css';
+
+// Diğer bileşenleri içe aktarın
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<SignIn></SignIn>} />
+        <Route exact path="/signup" element={<SignUp></SignUp>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/SignIn.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    return (
+        <div className='mcard-locat'>
+          <div className='m-card'>
+            <h1>Sign in</h1>
+            <p className='details-register-p'>Please enter your login and password!</p>
+    
+            <input className='get-input' onClick={()=> setEmail('')} placeholder='Email address' type='email' />
+            <input className='get-input' onClick={()=> setPassword('')} placeholder='Password' type='password' />
+    
+            <hr/>
+
+            <button className='button-log-reg' type='submit'>Login</button>
+    
+            <p className='white-p'>Don't have an account? <Link to="/signup" className='g-link'>Register here</Link></p>
+            <p className='details-register-p'>To sign in you need to have premium spotify account</p>
+          </div>
+        </div>
+      );
+}
+
+export default SignIn;
+
+
