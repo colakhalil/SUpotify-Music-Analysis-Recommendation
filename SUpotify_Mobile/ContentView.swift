cb535583e5a306d98dbd1074672b3dadf2a0aef4
hiDeniz <halilibrahimdeniz2001@gmail.com>
2023-10-31 16:48:37 +0300

    Initial Commit

---
+++ SUpotify_Mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUpotify_Mobile
+//
+//  Created by Halil İbrahim Deniz on 31.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUpotify_Mobile/SUpotify_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUpotify_MobileApp.swift
+//  SUpotify_Mobile
+//
+//  Created by Halil İbrahim Deniz on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUpotify_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
