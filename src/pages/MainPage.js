556136a4a3339d66b0a560e4328dde5b518c9fb3
colakhalil <colakhalil@sabanciuniv.edu>
2023-11-06 18:17:16 +0300

    All the left right and bottom bars finalized -halil

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -20,8 +20,57 @@ const MainPage = () => {
     img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
   };
 
+  const friendsData = [
+    {
+      name: "Idil Güler",
+      lastListenedSong: "Song Name 1",
+      profilePicture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+    },
+    {
+      name: "Umit Colak",
+      lastListenedSong: "Song Name 2",
+      profilePicture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+    },
+    {
+      name: "Halil Colak",
+      lastListenedSong: "Song Name 3",
+      profilePicture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+    },
+    // Add more friend objects
+  ];
+  // Dummy data for playlists
+  const playlists = [
+    {
+      playlist_name: "EFKARLI IKEN",
+      playlist_picture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+      song_number: "12949129412",
+    },
+    {
+      playlist_name: "GAZLAAAAAAA",
+      playlist_picture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+      song_number: "124124",
+    },
+    {
+      playlist_name: "HANIMI DUSUNURKEN",
+      playlist_picture:
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/artistic-album-cover-design-template-d12ef0296af80b58363dc0deef077ecc_screen.jpg?ts=1696331695",
+      song_number: "9999999999",
+    },
+    // Add more playlists here...
+  ];
   // when song is updated this BottomBar also will be updated automatically
-  return <BottomBar song={song} />;
+  return (
+    <>
+      <BottomBar song={song} />
+      <FriendBar friendsData={friendsData} />
+      <LeftBar playlists={playlists} />
+    </>
+  );
 };
 
 const BottomBar = ({ song }) => {
@@ -30,6 +79,7 @@ const BottomBar = ({ song }) => {
   // Function to handle when the user changes their rating
   const handleRatingChange = (newRating) => {
     setUserRating(newRating);
+    console.log("NEW RATING: ", newRating);
 
     // REQUEST!!!!!!! USER WILL CHANGE RATING ON THE SONG
   };
@@ -67,8 +117,7 @@ const BottomBar = ({ song }) => {
   // Toggle play/pause
   const togglePlay = () => {
     setIsPlaying(!isPlaying);
-    // REQUEST!!!!!!!  LAST CLICKED SONG WILL BE CHANGED
-    // REQUEST!!!!!!!  PLAYS NUMBER WILL BE INCREASED
+    // REQUEST!!!!!!!  USER'S LAST CLICKED SONG WILL BE CHANGED
   };
 
   // This will reset the timer and stop playing when the song duration is reached
@@ -109,7 +158,16 @@ const BottomBar = ({ song }) => {
         <button
           className="song-lyrics-btn"
           onClick={toggleLyrics}
-          style={{ marginLeft: "2rem" }}
+          style={{
+            marginLeft: "2rem",
+            padding: "10px 20px",
+
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            transition: "background-color 0.3s ease-in-out",
+          }}
         >
           Lyrics
         </button>
@@ -147,4 +205,72 @@ const BottomBar = ({ song }) => {
     </div>
   );
 };
+
+const FriendBar = ({ friendsData }) => {
+  return (
+    <div className="friend-bar">
+      {friendsData.map((friend, index) => (
+        <div className="friend" key={index}>
+          <img
+            src={friend.profilePicture}
+            alt={friend.name}
+            className="friend-picture"
+          />
+          <div className="friend-info">
+            <p className="friend-name">{friend.name}</p>
+            <p className="last-song">{friend.lastListenedSong}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+const LeftBar = ({ playlists }) => {
+  const handleClick = (playlistName) => {
+    // Here you can handle the navigation or any other interaction
+    console.log(`You clicked on ${playlistName}`);
+    //REQUEST PLAYLIST SONGS SHOULD BE SEEN IN THE MAIN
+  };
+  const handleMain = () => {
+    // Here you can handle the navigation or any other interaction
+    //REQUEST USER SHOULD NAVIGATE TO MAIN
+  };
+  const handleSearch = () => {
+    // Here you can handle the navigation or any other interaction
+    //REQUEST USER SHOULD NAVIGATE TO SEARCH
+  };
+
+  return (
+    <div className="left-bar">
+      <button className="left-bar-button" onClick={handleMain}>
+        Ana sayfa
+      </button>
+      <button className="left-bar-button" onClick={handleSearch}>
+        Ara
+      </button>
+      <button className="left-bar-button">Kitaplığın</button>
+
+      <div className="playlists">
+        {playlists.map((playlist, index) => (
+          <button
+            key={index}
+            className="playlist-button"
+            onClick={() => handleClick(playlist.playlist_name)}
+          >
+            <img
+              src={playlist.playlist_picture}
+              alt={playlist.playlist_name}
+              className="playlist-image"
+            />
+            <div className="playlist-info">
+              <div className="playlist-name">{playlist.playlist_name}</div>
+              <div className="song-number">{playlist.song_number} songs</div>
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default MainPage;
