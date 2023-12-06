b97837eea2f5d5b392914d476299de29cde9e9b4
İdil Güler <idilguler@sabanciuniv.edu>
2023-12-07 00:22:28 +0300

    I prepared it for merge-idil

--- src/global.js
+++ src/global.js
@@ -1,6 +1,7 @@
 // This is an example of a global variable
 const globalVar = {
-  mail: "yusuferen2329@gmail.com",
+  mail: "idililguler2002@gmail.com",
+  username:"idilguler"
 };
 
 export default globalVar;
