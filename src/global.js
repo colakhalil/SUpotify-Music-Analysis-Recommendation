7798f0c15a2193ea241b6f82e1b16b8cd111b9c7
umitcolak <umit2002colak@gmail.com>
2023-12-11 04:01:44 +0300

    as

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 // This is an example of a global variable
 const globalVar = {
   mail: "umit2002colak@gmail.com",
-  username: "umitcolak"
+  username: "umitcolakk"
 };
 
 export default globalVar;
